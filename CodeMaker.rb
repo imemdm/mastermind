25a900ea1dfd20584fe70b24c95dc162064a21cc
Emil Dimitrov <edimitrov1@gmail.com>
2018-10-18 15:47:51 +0300

    Extract method in CodeMaker to make successive additions easier

--- CodeMaker.rb
+++ CodeMaker.rb
@@ -1,18 +1,27 @@
 class CodeMaker
   attr_reader :possibilites
+  attr_accessor :pattern
 
   def initialize
     @possibilites = [0, 1, 2, 3, 4, 5, 6]
+    @pattern = nil
   end
 
-  # Generates a pattern with 4 random elements
-  # picked from @possibilites
+  # Generates a code with 4 random elements from @possibilites
   def code
-    pattern = []
+    generate_random_code
+  end
+
+  def feedback(break_pattern)
+
+  end
+
+  def generate_random_code
+    elements = []
     4.times do
       p = rand(4)
-      pattern << self.possibilites[p]
+      elements << self.possibilites[p]
     end
-    pattern
+    self.pattern = elements
   end
 end
