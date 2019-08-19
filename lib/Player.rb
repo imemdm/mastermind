8b0b5c0e6aa6ff0bff2af3161cea561350becbea
Emil Dimitrov <edimitrov1@gmail.com>
2019-08-19 14:58:27 +0300

    Player can be a code maker

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,9 +1,19 @@
 class Player
   attr_accessor :total_points
-  attr_reader :name
+  attr_reader :status
   
-  def initialize(name)
-    @name = name
+  def initialize(status)
+    @status = status
     @total_points = 0
+  end
+
+  def make_code
+    maker.code
+  end
+
+  private
+
+  def maker
+    CodeMaker.new(:status)
   end
 end
