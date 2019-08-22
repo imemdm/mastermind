6c76c0c1285b14e186c4b02e9d14664c933aa467
Emil Dimitrov <edimitrov1@gmail.com>
2019-08-22 18:50:31 +0300

    Can get single valid digit

--- lib/InputHandler.rb
+++ lib/InputHandler.rb
@@ -1,4 +1,19 @@
 class InputHandler
   def self.make_code
+    puts "Make a code:\n"
+
+  end
+
+  def self.get_digit
+    digit = -1
+    until valid_digit?(digit)
+      print "Enter a digit (0-5): "
+      digit = gets.chomp.to_i 
+    end
+    digit
+  end
+
+  def self.valid_digit?(digit)
+    digit.between?(0, 5)
   end
 end
