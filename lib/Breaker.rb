cbb784be48e3af1c4f4f204621ceb6bc8abf9a28
Emil Dimitrov <edimitrov1@gmail.com>
2019-08-22 20:29:29 +0300

    Remove redundnat guess make from breaker

--- lib/Breaker.rb
+++ lib/Breaker.rb
@@ -16,26 +16,6 @@ class Breaker
   private
 
   attr_writer :current_guess
-  
-  # Handles a single guess
-  def single(pos)
-    print "Guess position #{pos}: "
-    gets.chomp.to_i
-  end
-
-  # Handles the guess process from a human player's perspective
-  def handle_input
-    gs = []
-    4.times do |i|
-      g = nil
-      loop do
-        g = single(i + 1)
-        break if valid_guess?(g)
-      end
-      gs << g
-    end
-    gs
-  end
 
   # Required logic for the computer to make a guess
   def generate_guess(old_data)
@@ -46,10 +26,5 @@ class Breaker
       code = ai.attempt(old_data)
     end
     code
-  end
-
-  # Checks each guess for validity
-  def valid_guess?(input)
-    input >= 0 && input <= 5
   end
 end
